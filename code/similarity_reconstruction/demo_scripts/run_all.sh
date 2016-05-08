64d0b61c9018b3f6c5b111c0ee16da1ca1ee0595
czhou <zhouch08@gmail.com>
2016-05-09 00:34:14 +0800

    clean

--- code/similarity_reconstruction/demo_scripts/run_all.sh
+++ code/similarity_reconstruction/demo_scripts/run_all.sh
@@ -9,5 +9,5 @@ run_joint_opt=1
 . ./run_reconstruction_both_sides.sh
 . ./run_crop_tsdf_as_gt_in_refactorcode.sh
 detector_train_data_dir=$train_data_dir_computed
-. ./run_detection_hdin.sh
-. ./run_joint_reconstruction-hdin.sh
+. ./run_detection.sh
+. ./run_joint_reconstruction.sh
